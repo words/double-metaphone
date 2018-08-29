8cd5115501f7453c7e42ad1a9214a8cff3089bcf
Titus Wormer <tituswormer@gmail.com>
2018-08-29 20:45:49 +0200

    Move tests to `test.js`

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 'use strict'
 
+var PassThrough = require('stream').PassThrough
 var assert = require('assert')
 var test = require('tape')
-var m = require('..')
+var execa = require('execa')
+var version = require('./package.json').version
+var m = require('.')
 
 test('api', function(t) {
   t.equal(typeof m, 'function', 'should be a `function`')
@@ -763,4 +766,42 @@ test('api', function(t) {
   t.equal(m('z')[0], 'S', 'should transform Z to S')
 
   t.end()
+})
+
+test('cli', function(t) {
+  var input = new PassThrough()
+  var helps = ['-h', '--help']
+  var versions = ['-v', '--version']
+
+  t.plan(7)
+
+  execa.stdout('./cli.js', ['michael']).then(function(result) {
+    t.equal(result, 'MKL	MXL', 'argument')
+  })
+
+  execa.stdout('./cli.js', ['detestable', 'vileness']).then(function(result) {
+    t.equal(result, 'TTSTPL\tTTSTPL FLNS\tFLNS', 'arguments')
+  })
+
+  execa.stdout('./cli.js', {input: input}).then(function(result) {
+    t.equal(result, 'TTSTPL\tTTSTPL FLNS\tFLNS', 'stdin')
+  })
+
+  input.write('detestable')
+
+  setImmediate(function() {
+    input.end(' vileness')
+  })
+
+  helps.forEach(function(flag) {
+    execa.stdout('./cli.js', [flag]).then(function(result) {
+      t.ok(/\s+Usage: double-metaphone/.test(result), flag)
+    })
+  })
+
+  versions.forEach(function(flag) {
+    execa.stdout('./cli.js', [flag]).then(function(result) {
+      t.equal(result, version, flag)
+    })
+  })
 })
--- test/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-/* eslint-disable import/no-unassigned-import */
-require('./api')
-require('./cli')
